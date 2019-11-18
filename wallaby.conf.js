02004f3b1eb60740da0510f9dcaf639c3e53bd5e
DarrenPaulWright <darren@darrenpaulwright.com>
2019-11-18 15:57:01 -0800

    Adding karma-webpack-bundle; updating dependencies

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,38 +1,6 @@
-const wallabyWebpack = require('wallaby-webpack');
-const testRunnerConfig = require('test-runner-config');
-const config = require('./testRunner.config.js');
+const wallabyConfig = require('karma-webpack-bundle').wallabyConfig;
+const testRunnerConfig = require('./testRunner.config.js');
 
-const files = testRunnerConfig.getWallabyFiles(config, {
-	helper: (file) => {
-		return {pattern: file, instrument: false, load: false};
-	},
-	src: (file) => {
-		return {pattern: file, instrument: true, load: false};
-	},
-	specs: (file) => {
-		return {pattern: file, instrument: false, load: false};
-	}
+module.exports = wallabyConfig(testRunnerConfig, {
+	name: 'object-agent'
 });
-
-module.exports = function(wallaby) {
-	const webpackPostprocessor = wallabyWebpack();
-
-	return {
-		name: 'object-agent',
-		files: files.files,
-		tests: files.tests,
-		testFramework: 'mocha',
-		env: {
-			kind: 'chrome'
-		},
-		postprocessor: webpackPostprocessor,
-		compilers: {
-			'**/*.js': wallaby.compilers.babel()
-		},
-		setup: function() {
-			window.__moduleBundler.loadTests();
-		},
-		// debug: true,
-		lowCoverageThreshold: 99
-	};
-};
