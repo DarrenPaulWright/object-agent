7260ea1f350088b77214175ac3716664fcd90ec4
DarrenPaulWright <darren@darrenpaulwright.com>
2022-06-06 21:25:19 -0700

    Simplify Wallaby config

--- wallaby.conf.cjs
+++ wallaby.conf.cjs
@@ -1,5 +1,4 @@
 const { wallabyConfig } = require('karma-webpack-bundle');
+const { name } = require('./package.json');
 
-module.exports = wallabyConfig(undefined, {
-	name: 'object-agent'
-});
+module.exports = wallabyConfig(undefined, { name });
